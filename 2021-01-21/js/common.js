8b63b34efaf7e7f64e059ed41207d0488edc367c
Jeong-jeong <ghkdvnfld345@naver.com>
2021-01-18 14:19:52 +0900

    Docs: 로그인 시 메인페이지 변화 구현
    - .login 추가시 변화
    - #top_event css 변경
    - .sign_menu 회원가입 버튼 삭제,
    유저 sub 메뉴 구현
    
    <issue>
    - 로그인된 userName 받아와야 함
    - border가 2px로 표시됨

--- 2021-01-21/js/common.js
+++ 2021-01-21/js/common.js
@@ -1,10 +1,15 @@
 'use strict';
 
+
+// 로그인 시 top_event, sign_menu .login 추가
+
+
+
+// gnb 상단 고정 구현
 const gnb = document.querySelector('.gnb');
 const gnbTopOffset = gnb.offsetTop;
 window.addEventListener('scroll', e =>{
 
-    // .gnb 상단에 고정
     if (window.pageYOffset >= gnbTopOffset) {
         gnb.style.position = 'fixed';
         gnb.style.top = 0;
@@ -15,3 +20,4 @@ window.addEventListener('scroll', e =>{
         gnb.style.top = '';
     }
 });
+// gnb 상단 고정 끝 --
