dc043be4cb4dd142d255029ff079eaf97444f0f7
Jeong-jeong <ghkdvnfld345@naver.com>
2021-01-19 21:43:21 +0900

    Style: gnb_search js 추가
    - btn_delete 클릭 > input clear
    - gnb_search focus 시 배경색 #fff로 변경, transtion 추가, focus 잃으면 원래대로.

--- 2021-01-21/js/common.js
+++ 2021-01-21/js/common.js
@@ -21,3 +21,24 @@ window.addEventListener('scroll', e =>{
     }
 });
 // gnb 상단 고정 끝 --
+
+
+// gnb_search
+// (1) btn_delete 클릭 > value 값 초기화
+const btnD = document.querySelector('.btn_delete');
+const inpSearch = document.querySelector('.inp_search');
+
+btnD.onclick = function () {
+    inpSearch.value = '';
+}
+
+// (2) 입력 창 focus > 배경 #fff 변경, focus를 잃으면 원래대로.
+// transition 0.3초 추가
+inpSearch.addEventListener('focus', function(changeBg) {
+    this.style.backgroundColor = '#fff';
+}, true);
+inpSearch.addEventListener('blur', function(changeBg) {
+    this.style.backgroundColor = '';
+}, true);
+
+
