be97d575f27b8acb48ebccb3b126d5e4c4983484
Jeong-jeong <ghkdvnfld345@naver.com>
2021-02-22 14:17:20 +0900

    Update file
    21.2.24

---
+++ 2021-02-24/js/contents.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// #go-top smooth 적용이 안됨 ㅠ
+// a 태그에 #값을 넣으면 top으로 가지만 smooth 적용 안됐던 것. > button으로 변경
+let goTop = () => {
+  // Scroll | button show/hide
+  window.addEventListener('scroll', () => {
+    if (document.querySelector('html, body').scrollTop > 1000) {
+      document.getElementById('go-top').style.opacity = "1";
+    } else {
+      document.getElementById('go-top').style.opacity = "0";
+    }
+  });
+  // back to top
+  document.getElementById('go-top').addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  })
+};
+goTop();
+
+// floating banner (.qnb)
+// 현재 스크롤이 이동한 높이를 배너의 높이에 적용
+// https://m.blog.naver.com/PostView.nhn?blogId=pjh445&logNo=220261440747&proxyReferer=https:%2F%2Fwww.google.com%2F
+// https://mastmanban.tistory.com/600
---
+++ 2021-02-24/main_mobile/README.md
@@ -0,0 +1,10 @@
+# 모바일 버젼 기본 템플릿입니다!    
+
+mobile.html / reset.css / common_mobile.css /common_mobile.js 이고 적용된 상태는 아래와 같습니다    
+
+이 상태에서 수정하셔서 사용하시면 돼요!   코드에 모바일버젼 불러오는법 , 보라색테마 쓰는법 , 흰색테마 쓰는법 다 적어뒀습니다!
+
+![흰색](https://user-images.githubusercontent.com/64634992/105448201-32727880-5cb9-11eb-95fc-595d85acd810.png) ![보라색](https://user-images.githubusercontent.com/64634992/105448194-2e465b00-5cb9-11eb-9265-aefce9d1e6fb.png)
+
+
+
